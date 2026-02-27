21ed83990ef63ab618a9f3dd9433f8dd627bab69
Dieme- Musabyimana <musabyimanadieme10@gmail.com>
2026-02-27 02:05:25 +0200

    test

--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class SortingTest {
-
+//Start test
     @Test
     void validateAllSortingOptions() {
         try (Playwright playwright = Playwright.create()) {
